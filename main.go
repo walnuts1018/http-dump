43dad97248e31b0aa598f7ebf0ab8eca37fe2c23
walnuts1018 <r.juglans.1018@gmail.com>
2024-01-12 21:36:06 +0900

    Update logging to use custom logger

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -14,19 +14,18 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		d, err := httputil.DumpRequest(r, true)
 		if err != nil {
-			msg := fmt.Sprintf("couldn't dump request: %s", err)
-			log.Printf(msg)
+			msg := fmt.Sprintf("couldn't dump request: %v", err)
+			slog.Error(msg, r.Host, r.URL.Path, r.Method, r.RemoteAddr)
 			http.Error(w, msg, http.StatusInternalServerError)
 			return
 		}
 
 		b := string(d)
 
-		log.Printf("request received:\n%s\n\n", b)
-
-		if _, err := fmt.Fprintf(w, b); err != nil {
+		slog.Info(fmt.Sprintf("request received:\n%s\n\n", b), r.Host, r.URL.Path, r.Method, r.RemoteAddr)
+		if _, err := fmt.Fprint(w, b); err != nil {
 			msg := fmt.Sprintf("couldn't write response: %s", err)
-			log.Printf(msg)
+			slog.Error(msg, r.Host, r.URL.Path, r.Method, r.RemoteAddr)
 			http.Error(w, msg, http.StatusInternalServerError)
 			return
 		}
@@ -38,6 +37,9 @@ func main() {
 
 	addr := ":" + port
 
-	log.Printf("http-dump is listening at %s\n", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	slog.Info(fmt.Sprintf("http-dump is listening at %s\n", addr))
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		slog.Error(fmt.Sprintf("couldn't start server: %s", err))
+	}
+	slog.Info("http-dump is shutting down")
 }
